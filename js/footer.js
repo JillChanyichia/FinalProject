78ab3b5a337ace93e7678d1aa46b0c4f5c191ef0
JillChanyichia <38992092+JillChanyichia@users.noreply.github.com>
2019-12-12 00:49:36 -0500

    File_Change

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+footer ='<footer>\
+    <h6>Self-Designed and Developed © Bootlegway 2019</h6>\
+</footer>'
+
+document.write(footer);
+
+
---
+++ js/nav.js
@@ -0,0 +1,24 @@
+header ='<nav class="navbar navbar-expand-lg navbar-light fixed-top my_navbar" style="padding-left: 0em; padding-right: 0em;">\
+<a class="logo" href="index.html">Bootlegway</a>\
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">\
+            <span class="navbar-toggler-icon"></span>\
+    </button>\
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">\
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">\
+            <li class="nav-item active ">\
+                <a class="nav-link text" href="shop.html">APPAREL<span class="sr-only">(current)</span></a>\
+            </li>\
+            <li class="nav-item active ">\
+                <a class="nav-link text" href="select.html">SELECT<span class="sr-only">(current)</span></a>\
+            </li>\
+           \
+            <li class="nav-item text last">\
+                <a class="nav-link" href="about.html">ABOUT</a>\
+            </li>\
+        </ul>\
+    </div>\
+</nav>'
+
+document.write(header);
+
+
